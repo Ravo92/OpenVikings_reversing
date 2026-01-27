75b0d691262969a297d2576906f60e3eea14837d
Raphael Vogel <ra.vo@hotmail.com>
2026-01-27 22:49:03 +0100

    Added EngineContext

---
+++ Source/Engine/EngineContext.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenVikings.Engine
+{
+    internal sealed class EngineContext
+    {
+        internal int ResolutionWidth { get; set; }
+        internal int ResolutionHeight { get; set; }
+        internal int ColorDepthBits { get; set; }
+
+        internal bool CustomCursorEnabled { get; set; }
+        internal nint CursorHandle { get; set; }
+
+        internal int State { get; set; }
+    }
+
+}
